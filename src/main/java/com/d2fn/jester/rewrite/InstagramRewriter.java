2cc76b6166d6bd4b661b5b2cff4e3314e5afd2ca
Collin Van Dyck <collinvandyck@gmail.com>
2012-10-02 17:17:02 -0700

    Add support for slightly shortened instagram links

--- src/main/java/com/d2fn/jester/rewrite/InstagramRewriter.java
+++ src/main/java/com/d2fn/jester/rewrite/InstagramRewriter.java
@@ -7,7 +7,7 @@ import java.util.regex.Pattern;
 import static java.util.regex.Pattern.compile;
 
 public class InstagramRewriter extends AbstractImageRewriter {
-    public static Pattern LINK_PATTERN = compile("(http://)?(www\\.)?instagram[^\\s]+");
+    public static Pattern LINK_PATTERN = compile("(http://)?(www\\.)?instagr.?am[^\\s]+");
     public static Pattern EMBEDDED_LINK_PATTERN = compile("img class=\"photo\".*(http://[^\"]+)", Pattern.MULTILINE);
 
     public InstagramRewriter(HttpClient httpClient) {
